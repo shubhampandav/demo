2189ac28092ccedb97bbbc9acb6bec3173e437e3
shubhampandav <shubhampandav1479@gmail.com>
2022-03-13 10:28:48 +0530

    commit

--- demo.php
+++ demo.php
@@ -1,15 +1,19 @@
 <?php
-// Declaring variables
-$txt = "Hello World!";
-$number = 10;
  
-// Displaying variables value
-echo $txt;  // Output: Hello World!
-echo $number; // Output: 10
-?>
+ //declare variable in php
+ $name = "shubham";
+ $number = 23;
 
-<?php
-$name= "shubham pandav ";
-echo "$name";
+ // to print the output we use 
+ echo"$name";
+ echo"$number";
+
+ // adding two Numbers  in php 
+ $num1 = 56;
+ $num2 = 34;
+
+ $add = $num1 + $num2;
+ echo"$add";
+
 
 ?>
