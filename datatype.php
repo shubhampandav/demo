9b11cd3ece192710cf223effd795cfb620977150
shubhampandav <shubhampandav1479@gmail.com>
2022-03-13 11:26:55 +0530

    commit

---
+++ datatype.php
@@ -0,0 +1,24 @@
+<?php
+//data types in php 
+// String
+//integer 
+//float
+//boolean 
+//array
+//objects
+
+$var = "This is string";
+echo var_dump($var);
+echo"<br>";
+
+$num = 78;
+echo var_dump($num);
+echo"<br>";
+
+$num = 78.56;
+echo var_dump($num);
+echo"<br>";
+
+$num = true;
+echo var_dump($num);
+echo"<br>";
